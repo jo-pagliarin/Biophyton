34f679fcac83f5c8df8869a815f3400112751fca
JoanaPagliarin <joanapagliarin5@gmail.com>
2022-10-07 16:26:13 -0300

    retomar

---
+++ Iniciando em python/modulos.py
@@ -0,0 +1,23 @@
+from importlib.metadata import packages_distributions
+import math
+from struct import pack
+a = 25
+print(math.sqrt(25))
+
+from math import pow, sqrt
+b = 2
+c = 4
+print(pow(2,4))
+
+d = 81
+raiz = sqrt(d)
+print("A raiz de {} é igual a {:.2f}".format(d,raiz))
+print("A raiz de {} é igual a {}".format(d,raiz))
+
+import random # biblioteca que gera números aleatórios
+num = random.random()
+print(num)
+
+num = random.randint(1,10)
+print(num)
+
