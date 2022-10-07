969a0a0b2a0d2b2ccbbc767801c59f2c52b2c0d4
JoanaPagliarin <joanapagliarin5@gmail.com>
2022-10-07 20:28:18 -0300

    fazer uma função para imprimir classe

--- Iniciando em python/funcao.py
+++ Iniciando em python/funcao.py
@@ -1,17 +1,21 @@
-class aluno:
+class Aluno:
     def __init__(self, nome, nota1, nota2, situacao):
-        self.nome = nome 
+        self.nome = nome
         self.nota1 = nota1
         self.nota2 = nota2
         self.situacao = ["aprovado", "reprovado", "em recuperação"]
 
-n = 5
+
+n = 3
+
+alunos = []
 
 def leAluno(n):
     i = 0
-    while(i<n):
-        a  = aluno(input("Nome do aluno: "), input("Nota 1: "), input("Nota 2: "), input("Situação: "))
-        aluno.append(a)
-        i += 1 
-    
+    while (i < n):
+        aux = Aluno(input("Nome do aluno: "), input("Nota 1: "),
+                    input("Nota 2: "), input("Situação: "))
+        alunos.append(aux)
+        i += 1
+
 leAluno(n)
